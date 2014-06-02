b7ebdf7d27c4a90e56cdb1da6b12d3f55e884411
Chehab Mustafa-Hilmy <m@chehab.me>
2014-06-02 12:52:02 +0000

    [FIX] jQ manupulation fixes for slidesNav.

--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -7,9 +7,19 @@ $(document).ready(function() {
         animateAnchor: true,
         verticalCentered: true,
         resize : true,
+        loopHorizontal: false,
     });
     $.fn.fullpage.moveTo(0,4); 
     $("#full-page .controlArrow.prev").append('<i class="fa fa-chevron-left fa-3x"></i>');
     $("#full-page .controlArrow.next").append('<i class="fa fa-chevron-right fa-3x"></i>');
-    $(".fullPage-slidesNav").css("margin-left", "0px"); 
+    $(".fullPage-slidesNav").css("margin-left", "0px");
+    
+    // fullPage-slidesNav
+    $(".fullPage-slidesNav > ul").addClass('nav nav-pills')
+    $(".fullPage-slidesNav > ul li:nth-child(1) a").empty().prepend("Contacts");
+    $(".fullPage-slidesNav > ul li:nth-child(2) a").empty().prepend("Psych Tests");
+    $(".fullPage-slidesNav > ul li:nth-child(3) a").empty().prepend("Problems");
+    $(".fullPage-slidesNav > ul li:nth-child(4) a").empty().prepend("Services");
+    $(".fullPage-slidesNav > ul li:nth-child(5) a").empty().prepend("About");
+
 });

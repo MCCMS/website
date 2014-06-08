dff2fd39ffd0da5d2c3f46f27abbed51b37004ca
Chehab AbdElHamed <m@chehab.me>
2014-06-08 14:33:28 +0200

    [NEW] change '#brand' text colour on different slide/backgound. [FIX] minore changes

--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -1,19 +1,37 @@
+function brand_darkBackgound(){
+      $('#brand .dm_title h2').switchClass('background-light','background-dark', 100, "easeInOutQuad");
+      $('#brand .dm_title ul').switchClass('background-light','background-dark', 100, "easeInOutQuad");
+};
+function brand_lightBackgound(){
+      $('#brand .dm_title h2').switchClass('background-dark', 'background-light', 100, "easeInOutQuad");
+      $('#brand .dm_title ul').switchClass('background-dark', 'background-light', 100, "easeInOutQuad");
+}
+
+
 $(document).ready(function() {
     $('#full-page').fullpage({
         menu: '#menu',
         slidesColor: ['#27ae60', '#2c3e50', '#e74c3c', '#27ae60', '#27ae60'],
-        slidesNavigation: true,			
+        slidesNavigation: true,
         slidesNavPosition: 'top pull-left',
         animateAnchor: true,
         verticalCentered: true,
         resize : false,
         loopHorizontal: false,
+
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+          console.log('==================================');
+          console.log('slide changeing');
+          console.log('slideIndex: '+slideIndex);
+          if( slideIndex == 2 ){ brand_darkBackgound();}
+          else{ brand_lightBackgound();}
+        },
     });
-    $.fn.fullpage.moveTo(0,4); 
+    $.fn.fullpage.moveTo(0,4);
     $("#full-page .controlArrow.prev").append('<i class="fa fa-chevron-left fa-3x"></i>');
     $("#full-page .controlArrow.next").append('<i class="fa fa-chevron-right fa-3x"></i>');
     $(".fullPage-slidesNav").css("margin-left", "0px");
-    
+
     // fullPage-slidesNav
     $(".fullPage-slidesNav > ul").addClass('nav nav-pills')
     $(".fullPage-slidesNav > ul li:nth-child(1) a").empty().prepend("Contacts");
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 /******************************************************/
 /** Services Col. Hover-Zome **/
 
-var enlarsge = function(){
-    $(this).switchClass('','hover', 50);
-    $(this).find("p").switchClass('','lead',500);
-    $(this).transition({ scale: 1.2}, 500);   
-}
+// var enlarsge = function(){
+//     $(this).switchClass('','hover', 50);
+//     $(this).find("p").switchClass('','lead',500);
+//     $(this).transition({ scale: 1.2}, 500);
+// }
 
-var shrink = function(){ 
-    $(this).transition({ scale: 1 }, 500);
-    $(this).find("p").switchClass('lead','', 500);
-    $(this).switchClass('hover','', 10);  
-}
+// var shrink = function(){
+//     $(this).transition({ scale: 1 }, 500);
+//     $(this).find("p").switchClass('lead','', 500);
+//     $(this).switchClass('hover','', 10);
+// }
 
-$('.service-col').hover(enlarsge,shrink)
+// $('.service-col').hover(enlarsge,shrink)
 
 /******************************************************/
